6428c90ca8abeb4a4612ff95ef882d9dc5c03659
zaragoza-xu <xuyiyan920@gmail.com>
2025-06-14 20:10:58 +0800

    exp8

--- exp8/broadcast.c
+++ exp8/broadcast.c
@@ -1,10 +1,21 @@
 #include "base.h"
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 extern ustack_t *instance;
 
 void broadcast_packet(iface_info_t *iface, const char *packet, int len)
 {
 	// TODO: broadcast packet 
-	fprintf(stdout, "TODO: broadcast packet.\n");
+	iface_info_t *p;
+	list_for_each_entry(p, &instance->iface_list, list)
+	{
+		if(p != iface)
+		{
+			char *new_pkt = malloc(len);
+			memcpy(new_pkt, packet, len);
+			iface_send_packet(p, new_pkt, len);
+		}
+	}
 }
